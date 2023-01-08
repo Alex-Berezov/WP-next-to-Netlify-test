6cc669d0e12112b50c34945403cd4c7e20a114c7
Aleksey Berezov <a.berezov@leningrad.io>
2023-01-08 23:46:32 +0400

    Finished content moved

--- next.config.js
+++ next.config.js
@@ -125,6 +125,356 @@ module.exports = {
         destination: '/for-twins-2-math-operations',
         permanent: true,
       },
+      {
+        source: '/grasshopper---variable-assignment-debug',
+        destination: '/grasshopper-variable-assignment-debug',
+        permanent: true,
+      },
+      {
+        source: '/enumerable-magic-1---true-for-all',
+        destination: '/enumerable-magic-1-true-for-all',
+        permanent: true,
+      },
+      {
+        source: '/thinkful---dictionary-drills-order-filler',
+        destination: '/thinkful-dictionary-drills-order-filler',
+        permanent: true,
+      },
+      {
+        source: '/grasshopper---create-the-rooms',
+        destination: '/grasshopper-create-the-rooms',
+        permanent: true,
+      },
+      {
+        source: '/be-concise-i---the-ternary-operator',
+        destination: '/be-concise-i-the-ternary-operator',
+        permanent: true,
+      },
+      {
+        source: '/grasshopper---terminal-game-combat-function',
+        destination: '/grasshopper-terminal-game-combat-function',
+        permanent: true,
+      },
+      {
+        source: '/ruby-metaprogramming-101---dynamic-method-calls',
+        destination: '/ruby-metaprogramming-101-dynamic-method-calls',
+        permanent: true,
+      },
+      {
+        source: '/coding-3min--jumping-dutch-act',
+        destination: '/coding-3min-jumping-dutch-act',
+        permanent: true,
+      },
+      {
+        source: '/lua-is-easy-lesson-2---control-structures',
+        destination: '/lua-is-easy-lesson-2-control-structures',
+        permanent: true,
+      },
+      {
+        source: '/lua-is-easy-lesson-1---the-basics',
+        destination: '/lua-is-easy-lesson-1-the-basics',
+        permanent: true,
+      },
+      {
+        source: '/fun-with-es6-classes-1---people-people-people',
+        destination: '/fun-with-es6-classes-1-people-people-people',
+        permanent: true,
+      },
+      {
+        source: '/grasshopper---bug-squashing',
+        destination: '/grasshopper-bug-squashing',
+        permanent: true,
+      },
+      {
+        source: '/training-js-7-if..else-and-ternary-operator',
+        destination: '/training-js-7-if-else-and-ternary-operator',
+        permanent: true,
+      },
+      {
+        source: '/grasshopper---terminal-game-1',
+        destination: '/grasshopper-terminal-game-1',
+        permanent: true,
+      },
+      {
+        source: '/grasshopper---terminal-game-turn-function',
+        destination: '/grasshopper-terminal-game-turn-function',
+        permanent: true,
+      },
+      {
+        source: '/easy-sql---ordering',
+        destination: '/easy-sql-ordering',
+        permanent: true,
+      },
+      {
+        source: '/learning-typescript.-basic-types',
+        destination: '/learning-typescript-basic-types',
+        permanent: true,
+      },
+      {
+        source: '/learning-typescript.-basic-types.-type-assertions',
+        destination: '/learning-typescript-basic-types-type-assertions',
+        permanent: true,
+      },
+      {
+        source: '/bash-basics---while-loop',
+        destination: '/bash-basics-while-loop',
+        permanent: true,
+      },
+      {
+        source: '/fix-the-bugs-syntax---my-first-kata',
+        destination: '/fix-the-bugs-syntax-my-first-kata',
+        permanent: true,
+      },
+      {
+        source: '/php-functions---pass-by-reference',
+        destination: '/php-functions-pass-by-reference',
+        permanent: true,
+      },
+      {
+        source: '/a--b',
+        destination: '/a-b',
+        permanent: true,
+      },
+      {
+        source: '/noobcode-02-tricky-questions--primitives-and-operator-precedence',
+        destination: '/noobcode-02-tricky-questions-primitives-and-operator-precedence',
+        permanent: true,
+      },
+      {
+        source: '/be-concise-ii---i-need-squares',
+        destination: '/be-concise-ii-i-need-squares',
+        permanent: true,
+      },
+      {
+        source: '/be-concise-iii---sum-squares',
+        destination: '/be-concise-iii-sum-squares',
+        permanent: true,
+      },
+      {
+        source: '/a-bugs-trilogy-episode-1-let-math.random;-decide-your-future',
+        destination: '/a-bugs-trilogy-episode-1-let-math-random-decide-your-future',
+        permanent: true,
+      },
+      {
+        source: '/improving-math.roundx',
+        destination: '/improving-math-roundx',
+        permanent: true,
+      },
+      {
+        source: '/implement-array.prototype.filter',
+        destination: '/implement-array-prototype-filter',
+        permanent: true,
+      },
+      {
+        source: '/grasshopper---array-mean',
+        destination: '/grasshopper-array-mean',
+        permanent: true,
+      },
+      {
+        source: '/invalid-login---bug-fixing-11',
+        destination: '/invalid-login-bug-fixing-11',
+        permanent: true,
+      },
+      {
+        source: '/enumerable-magic-23---filter-array-by-regex',
+        destination: '/enumerable-magic-23-filter-array-by-regex',
+        permanent: true,
+      },
+      {
+        source: '/enumerable-magic-24---array-rejection',
+        destination: '/enumerable-magic-24-array-rejection',
+        permanent: true,
+      },
+      {
+        source: '/enumerable-magic-26---grab-elements-while',
+        destination: '/enumerable-magic-26-grab-elements-while',
+        permanent: true,
+      },
+      {
+        source: '/enumerable-magic-15---flat-map-that-array',
+        destination: '/enumerable-magic-15-flat-map-that-array',
+        permanent: true,
+      },
+      {
+        source: '/shorter-concat-[reverse-longer]',
+        destination: '/shorter-concat-reverse-longer',
+        permanent: true,
+      },
+      {
+        source: '/car-crash--o`o',
+        destination: '/car-crash-oo',
+        permanent: true,
+      },
+      {
+        source: '/thinking--testing-a-and-b',
+        destination: '/thinking-testing-a-and-b',
+        permanent: true,
+      },
+      {
+        source: '/t.t.t.32-count-with-your-fingers',
+        destination: '/t-t-t-32-count-with-your-fingers',
+        permanent: true,
+      },
+      {
+        source: '/balanced-number-special-numbers-series-1-',
+        destination: '/balanced-number-special-numbers-series-1',
+        permanent: true,
+      },
+      {
+        source: '/thinkful---number-drills-quadratic-formula',
+        destination: '/thinkful-number-drills-quadratic-formula',
+        permanent: true,
+      },
+      {
+        source: '/hamming-distance---part-1-binary-codes',
+        destination: '/hamming-distance-part-1-binary-codes',
+        permanent: true,
+      },
+      {
+        source: '/chuck-norris-ii---one-punch',
+        destination: '/chuck-norris-ii-one-punch',
+        permanent: true,
+      },
+      {
+        source: '/array.prototype.reverse',
+        destination: '/array-prototype-reverse',
+        permanent: true,
+      },
+      {
+        source: '/for-kids-d-m-y-day-of-the-week.',
+        destination: '/for-kids-d-m-y-day-of-the-week',
+        permanent: true,
+      },
+      {
+        source: '/playing-cards-draw-order-–-part-1',
+        destination: '/playing-cards-draw-order-part-1',
+        permanent: true,
+      },
+      {
+        source: '/i-will-take-the-biggest-one-and-nothing-else.',
+        destination: '/i-will-take-the-biggest-one-and-nothing-else',
+        permanent: true,
+      },
+      {
+        source: '/chuck-norris-i---push-ups',
+        destination: '/chuck-norris-i-push-ups',
+        permanent: true,
+      },
+      {
+        source: '/small-enough---beginner',
+        destination: '/small-enough-beginner',
+        permanent: true,
+      },
+      {
+        source: '/thinkful---object-drills-quarks',
+        destination: '/thinkful-object-drills-quarks',
+        permanent: true,
+      },
+      {
+        source: '/google-dorking---validating-queries',
+        destination: '/google-dorking-validating-queries',
+        permanent: true,
+      },
+      {
+        source: '/computer-science-101---3-deques',
+        destination: '/computer-science-101-3-deques',
+        permanent: true,
+      },
+      {
+        source: '/computer-science-101---2-queues',
+        destination: '/computer-science-101-2-queues',
+        permanent: true,
+      },
+      {
+        source: '/computer-science-101---1-stacks',
+        destination: '/computer-science-101-1-stacks',
+        permanent: true,
+      },
+      {
+        source: '/fourseven',
+        destination: '/four-seven',
+        permanent: true,
+      },
+      {
+        source: '/holiday-iii---fire-on-the-boat',
+        destination: '/holiday-iii-fire-on-the-boat',
+        permanent: true,
+      },
+      {
+        source: '/duck-shoot---easy-version',
+        destination: '/duck-shoot-easy-version',
+        permanent: true,
+      },
+      {
+        source: '/the-fusc-function----part-1',
+        destination: '/the-fusc-function-part-1',
+        permanent: true,
+      },
+      {
+        source: '/birthday-ii---presents',
+        destination: '/birthday-ii-presents',
+        permanent: true,
+      },
+      {
+        source: '/[code-golf]-number-pyramid-series-1---sum-of-corners',
+        destination: '/code-golf-number-pyramid-series-1-sum-of-corners',
+        permanent: true,
+      },
+      {
+        source: '/help-suzuki-count-his-vegetables....',
+        destination: '/help-suzuki-count-his-vegetables',
+        permanent: true,
+      },
+      {
+        source: '/[code-golf]-one-line-task-pyramid-of-spheres',
+        destination: '/code-golf-one-line-task-pyramid-of-spheres',
+        permanent: true,
+      },
+      {
+        source: '/fun-with-lists-anymatch--allmatch',
+        destination: '/fun-with-lists-anymatch-allmatch',
+        permanent: true,
+      },
+      {
+        source: '/the-office-i---outed',
+        destination: '/the-office-i-outed',
+        permanent: true,
+      },
+      {
+        source: '/upstreamdownstream',
+        destination: '/upstream-downstream',
+        permanent: true,
+      },
+      {
+        source: '/find-heavy-ball---level-novice',
+        destination: '/find-heavy-ball-level-novice',
+        permanent: true,
+      },
+      {
+        source: '/coding-meetup-5---higher-order-functions-series---prepare-the-count-of-languages',
+        destination: '/coding-meetup-5-higher-order-functions-series-prepare-the-count-of-languages',
+        permanent: true,
+      },
+      {
+        source: '/networking-1012-identify-the-ipv4-address-class',
+        destination: '/networking-101-2-identify-the-ipv4-address-class',
+        permanent: true,
+      },
+      {
+        source: '/coding-meetup-2---higher-order-functions-series---greet-developers',
+        destination: '/coding-meetup-2-higher-order-functions-series-greet-developers',
+        permanent: true,
+      },
+      {
+        source: '/bingo--or-not-',
+        destination: '/bingo-or-not',
+        permanent: true,
+      },
+      {
+        source: '/ascii-shift-encryptiondecryption',
+        destination: '/ascii-shift-encryption-decryption',
+        permanent: true,
+      },
     ];
   },
   experimental: {
--- src/middleware.js
+++ src/middleware.js
@@ -71,4 +71,224 @@ export function middleware(request) {
   if (request.nextUrl.pathname.startsWith('/for-twins-2.-math-operations')) {
     return NextResponse.rewrite(new URL('/for-twins-2-math-operations', request.url));
   }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---variable-assignment-debug')) {
+    return NextResponse.rewrite(new URL('/grasshopper-variable-assignment-debug', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/enumerable-magic-1---true-for-all')) {
+    return NextResponse.rewrite(new URL('/enumerable-magic-1-true-for-all', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/thinkful---dictionary-drills-order-filler')) {
+    return NextResponse.rewrite(new URL('/thinkful-dictionary-drills-order-filler', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---create-the-rooms')) {
+    return NextResponse.rewrite(new URL('/grasshopper-create-the-rooms', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/be-concise-i---the-ternary-operator')) {
+    return NextResponse.rewrite(new URL('/be-concise-i-the-ternary-operator', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---terminal-game-combat-function')) {
+    return NextResponse.rewrite(new URL('/grasshopper-terminal-game-combat-function', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/ruby-metaprogramming-101---dynamic-method-calls')) {
+    return NextResponse.rewrite(new URL('/ruby-metaprogramming-101-dynamic-method-calls', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/coding-3min--jumping-dutch-act')) {
+    return NextResponse.rewrite(new URL('/coding-3min-jumping-dutch-act', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/lua-is-easy-lesson-2---control-structures')) {
+    return NextResponse.rewrite(new URL('/lua-is-easy-lesson-2-control-structures', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/lua-is-easy-lesson-1---the-basics')) {
+    return NextResponse.rewrite(new URL('/lua-is-easy-lesson-1-the-basics', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/fun-with-es6-classes-1---people-people-people')) {
+    return NextResponse.rewrite(new URL('/fun-with-es6-classes-1-people-people-people', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---bug-squashing')) {
+    return NextResponse.rewrite(new URL('/grasshopper-bug-squashing', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/training-js-7-if..else-and-ternary-operator')) {
+    return NextResponse.rewrite(new URL('/training-js-7-if-else-and-ternary-operator', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---terminal-game-1')) {
+    return NextResponse.rewrite(new URL('/grasshopper-terminal-game-1', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---terminal-game-turn-function')) {
+    return NextResponse.rewrite(new URL('/grasshopper-terminal-game-turn-function', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/easy-sql---ordering')) {
+    return NextResponse.rewrite(new URL('/easy-sql-ordering', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/learning-typescript.-basic-types')) {
+    return NextResponse.rewrite(new URL('/learning-typescript-basic-types', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/learning-typescript.-basic-types.-type-assertions')) {
+    return NextResponse.rewrite(new URL('/learning-typescript-basic-types-type-assertions', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/bash-basics---while-loop')) {
+    return NextResponse.rewrite(new URL('/bash-basics-while-loop', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/fix-the-bugs-syntax---my-first-kata')) {
+    return NextResponse.rewrite(new URL('/fix-the-bugs-syntax-my-first-kata', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/php-functions---pass-by-reference')) {
+    return NextResponse.rewrite(new URL('/php-functions-pass-by-reference', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/a--b')) {
+    return NextResponse.rewrite(new URL('/a-b', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/noobcode-02-tricky-questions--primitives-and-operator-precedence')) {
+    return NextResponse.rewrite(
+      new URL('/noobcode-02-tricky-questions-primitives-and-operator-precedence', request.url)
+    );
+  }
+  if (request.nextUrl.pathname.startsWith('/be-concise-ii---i-need-squares')) {
+    return NextResponse.rewrite(new URL('/be-concise-ii-i-need-squares', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/be-concise-iii---sum-squares')) {
+    return NextResponse.rewrite(new URL('/be-concise-iii-sum-squares', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/a-bugs-trilogy-episode-1-let-math.random;-decide-your-future')) {
+    return NextResponse.rewrite(new URL('/a-bugs-trilogy-episode-1-let-math-random-decide-your-future', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/improving-math.roundx')) {
+    return NextResponse.rewrite(new URL('/improving-math-roundx', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/implement-array.prototype.filter')) {
+    return NextResponse.rewrite(new URL('/implement-array-prototype-filter', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/grasshopper---array-mean')) {
+    return NextResponse.rewrite(new URL('/grasshopper-array-mean', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/invalid-login---bug-fixing-11')) {
+    return NextResponse.rewrite(new URL('/invalid-login-bug-fixing-11', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/enumerable-magic-23---filter-array-by-regex')) {
+    return NextResponse.rewrite(new URL('/enumerable-magic-23-filter-array-by-regex', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/enumerable-magic-24---array-rejection')) {
+    return NextResponse.rewrite(new URL('/enumerable-magic-24-array-rejection', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/enumerable-magic-26---grab-elements-while')) {
+    return NextResponse.rewrite(new URL('/enumerable-magic-26-grab-elements-while', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/enumerable-magic-15---flat-map-that-array')) {
+    return NextResponse.rewrite(new URL('/enumerable-magic-15-flat-map-that-array', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/shorter-concat-[reverse-longer]')) {
+    return NextResponse.rewrite(new URL('/shorter-concat-reverse-longer', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/car-crash--o`o')) {
+    return NextResponse.rewrite(new URL('/car-crash-oo', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/thinking--testing-a-and-b')) {
+    return NextResponse.rewrite(new URL('/thinking-testing-a-and-b', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/t.t.t.32-count-with-your-fingers')) {
+    return NextResponse.rewrite(new URL('/t-t-t-32-count-with-your-fingers', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/balanced-number-special-numbers-series-1-')) {
+    return NextResponse.rewrite(new URL('/balanced-number-special-numbers-series-1', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/thinkful---number-drills-quadratic-formula')) {
+    return NextResponse.rewrite(new URL('/thinkful-number-drills-quadratic-formula', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/hamming-distance---part-1-binary-codes')) {
+    return NextResponse.rewrite(new URL('/hamming-distance-part-1-binary-codes', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/chuck-norris-ii---one-punch')) {
+    return NextResponse.rewrite(new URL('/chuck-norris-ii-one-punch', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/array.prototype.reverse')) {
+    return NextResponse.rewrite(new URL('/array-prototype-reverse', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/for-kids-d-m-y-day-of-the-week.')) {
+    return NextResponse.rewrite(new URL('/for-kids-d-m-y-day-of-the-week', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/playing-cards-draw-order-–-part-1')) {
+    return NextResponse.rewrite(new URL('/playing-cards-draw-order-part-1', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/i-will-take-the-biggest-one-and-nothing-else.')) {
+    return NextResponse.rewrite(new URL('/i-will-take-the-biggest-one-and-nothing-else', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/chuck-norris-i---push-ups')) {
+    return NextResponse.rewrite(new URL('/chuck-norris-i-push-ups', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/small-enough---beginner')) {
+    return NextResponse.rewrite(new URL('/small-enough-beginner', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/thinkful---object-drills-quarks')) {
+    return NextResponse.rewrite(new URL('/thinkful-object-drills-quarks', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/google-dorking---validating-queries')) {
+    return NextResponse.rewrite(new URL('/google-dorking-validating-queries', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/computer-science-101---3-deques')) {
+    return NextResponse.rewrite(new URL('/computer-science-101-3-deques', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/computer-science-101---2-queues')) {
+    return NextResponse.rewrite(new URL('/computer-science-101-2-queues', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/computer-science-101---1-stacks')) {
+    return NextResponse.rewrite(new URL('/computer-science-101-1-stacks', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/fourseven')) {
+    return NextResponse.rewrite(new URL('/four-seven', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/holiday-iii---fire-on-the-boat')) {
+    return NextResponse.rewrite(new URL('/holiday-iii-fire-on-the-boat', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/duck-shoot---easy-version')) {
+    return NextResponse.rewrite(new URL('/duck-shoot-easy-version', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/the-fusc-function----part-1')) {
+    return NextResponse.rewrite(new URL('/the-fusc-function-part-1', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/birthday-ii---presents')) {
+    return NextResponse.rewrite(new URL('/birthday-ii-presents', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/[code-golf]-number-pyramid-series-1---sum-of-corners')) {
+    return NextResponse.rewrite(new URL('/code-golf-number-pyramid-series-1-sum-of-corners', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/help-suzuki-count-his-vegetables....')) {
+    return NextResponse.rewrite(new URL('/help-suzuki-count-his-vegetables', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/[code-golf]-one-line-task-pyramid-of-spheres')) {
+    return NextResponse.rewrite(new URL('/code-golf-one-line-task-pyramid-of-spheres', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/fun-with-lists-anymatch--allmatch')) {
+    return NextResponse.rewrite(new URL('/fun-with-lists-anymatch-allmatch', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/the-office-i---outed')) {
+    return NextResponse.rewrite(new URL('/the-office-i-outed', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/upstreamdownstream')) {
+    return NextResponse.rewrite(new URL('/upstream-downstream', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/find-heavy-ball---level-novice')) {
+    return NextResponse.rewrite(new URL('/find-heavy-ball-level-novice', request.url));
+  }
+  if (
+    request.nextUrl.pathname.startsWith(
+      '/coding-meetup-5---higher-order-functions-series---prepare-the-count-of-languages'
+    )
+  ) {
+    return NextResponse.rewrite(
+      new URL('/coding-meetup-5-higher-order-functions-series-prepare-the-count-of-languages', request.url)
+    );
+  }
+  if (request.nextUrl.pathname.startsWith('/networking-1012-identify-the-ipv4-address-class')) {
+    return NextResponse.rewrite(new URL('/networking-101-2-identify-the-ipv4-address-class', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/coding-meetup-2---higher-order-functions-series---greet-developers')) {
+    return NextResponse.rewrite(
+      new URL('/coding-meetup-2-higher-order-functions-series-greet-developers', request.url)
+    );
+  }
+  if (request.nextUrl.pathname.startsWith('/bingo--or-not-')) {
+    return NextResponse.rewrite(new URL('/bingo-or-not', request.url));
+  }
+  if (request.nextUrl.pathname.startsWith('/ascii-shift-encryptiondecryption')) {
+    return NextResponse.rewrite(new URL('/ascii-shift-encryption-decryption', request.url));
+  }
 }
